708d8dc09b86141a1680d0df23898cf10cc4820a
Kasper Saaby <kdsaaby@gmail.com>
2014-08-25 14:37:55 +0200

    Implemented SOCK_Acceptor

---
+++ wrapper_facades/SOCK_Acceptor.cpp
@@ -0,0 +1,25 @@
+#include "SOCK_Acceptor.h"
+#include "INET_Addr.h"
+#include "SOCK_Stream.h"
+#include <Windows.h>
+
+// Initialize a passive-mode acceptor socket.
+SOCK_Acceptor::SOCK_Acceptor(const INET_Addr &addr) {
+	// Create a local endpoint of communication.
+	handle_ = socket(PF_INET, SOCK_Stream, 0);
+
+	// Associate address with endpoint.
+	bind(handle_, addr.addr(), addr.size());
+
+	// Make endpoint listen for connections.
+	listen(handle_, 5);
+}
+
+// A second method to initialize a passive-mode
+// acceptor socket, analogously to the constructor.
+void open(const INET_Addr &sock_addr) { /* ... */ }
+
+// Accept a connection and initialize the <stream>.
+void accept(SOCK_Stream &s) {
+	s.set_handle(accept(handle_, 0, 0));
+}
