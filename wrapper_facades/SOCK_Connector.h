6802c411cceadee67b03fb407da2b78736d4721f
RuneHeick <hteml@hotmail.com>
2014-08-26 14:41:00 +0200

    fix send

--- wrapper_facades/SOCK_Connector.h
+++ wrapper_facades/SOCK_Connector.h
@@ -12,18 +12,7 @@ public:
 	// connect to a socket; 
 	bool connect(INET_Addr addr);
 
-	//send a message to the connected 
-	// return number of byte send. 
-	int send(char *buffer, u_int size);
-
-	//revive a message 
-	//return the number of bytes recived. 
-	int recive(char* buffer, u_int maxSize);
-
 	SOCKET getSocket(); 
-
-	//Close the socket. 
-	void close(); 
 
 	bool isRunning = true; 
 
