ff6b97d85ee737bf5663797ed72cf7f880d85cee
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-08-26 14:34:05 +0200

    Client will repeatedly request server

--- client/client.cpp
+++ client/client.cpp
@@ -2,28 +2,37 @@
 
 #include <Windows.h>
 #include <iostream>
+#include <string>
 #include "stdafx.h"
 
 #include "INET_Addr.h"
 #include "SOCK_Connector.h"
-#include "SOCK_Stream.h"
 
 int _tmain(int argc, char* argv[])
 {
-	long ip = inet_addr(argv[0]);
-	u_short port = atoi(argv[1]);
+	long ip = 0x7F000001; // 127.0.0.1
+	u_short port = 5500;
 
 	INET_Addr address(port, ip);
 
-	SOCK_Connector client(address);
-	SOCK_Stream stream;
+	SOCK_Connector client();
+
+	client.connect(address);
 
 	std::cout << "Connected to " << ip << ":" << port << std::endl;
 
 	// Main client event loop.
 	for (;;)
 	{
-		stream = client.connect();
+		std::string clientName = "client";
+		client.send(clientName.c_str, clientName.length);
+
+		std::string response;
+		client.receive(response.c_str, 50);
+
+		std::cout << response << std::endl;
+
+		Sleep(2000);
 	}
 
 	return 0;
